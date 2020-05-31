bbd45803f65c6487f8a7c784bdab97b491398be0
Dylan <dylanconvery94@gmail.com>
2020-05-31 22:29:44 +0100

    designed entity and component classes

--- src/Game.hpp
+++ src/Game.hpp
@@ -9,18 +9,13 @@ class Game {
    public:
     Game();
     ~Game();
-
     bool init(int width, int height);
-
     void processInput();
     void update();
     void render();
-
     void destroy();
-
     bool loop() const;
 
-   public:
     uint32_t ticks_last_frame;
 
    private:
