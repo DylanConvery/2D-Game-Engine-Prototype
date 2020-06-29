2b0d067e3ac8b85b3dd06410c74c16a448521f27
Dylan <dylanconvery94@gmail.com>
2020-06-29 11:11:25 +0100

    Added a projectile emitter component. Allows you to instantiate projectiles. lua scripting wip

---
+++ assets/scripts/scene1.lua
@@ -0,0 +1,20 @@
+scene1 = {
+	assets = {
+		[0] = {type = "texture", id = "tank", file = "./assets/images/tank-big-right.png"},
+		[1] = {type = "texture", id = "chopper_spritesheet", file = "./assets/images/chopper-spritesheet.png"},
+		[2] = {type = "texture", id = "radar", file = "./assets/images/radar.png"},
+		[3] = {type = "texture", id = "jungle_tilemap", file = "./assets/tilemaps/jungle.png"},
+		[4] = {type = "texture", id = "helipad", file = "./assets/images/heliport.png"},
+		[5] = {type = "font", id = "charriot", file = "./assets/fonts/charriot.ttf", size = 16},
+		[6] = {type = "texture", id = "projectile", file = "./assets/images/bullet-enemy.png"},
+		[7] = {type = "texture", id = "bounding_box", file = "./assets/images/collision-texture.png"}
+	}
+	map = {
+		textureID = "",
+	}
+	entities = {
+		[0] = {
+			name = ""
+		},
+	}
+}
